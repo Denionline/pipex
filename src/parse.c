48264697353f73388e63240ee32d37b038f221eb
denionline <danielximenes164@gmail.com>
2025-08-11 11:38:17 +0100

    refactor: Simplify command handling and file setting logic in parse.c

--- src/parse.c
+++ src/parse.c
@@ -6,101 +6,68 @@
 /*   By: dximenes <dximenes@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/04 10:52:13 by dximenes          #+#    #+#             */
-/*   Updated: 2025/08/06 16:53:49 by dximenes         ###   ########.fr       */
+/*   Updated: 2025/08/11 11:35:28 by dximenes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-static char *get_valid_path(char **paths, char *command)
+static void	set_file(t_head *head, char *name, int pos)
 {
-	char	*complete_path;
-	char	*path_bar;
-	int		j;
-
-	j = 0;
-	while (paths[j])
+	if (!head->is_heredoc && pos == 0 && !access(name, R_OK))
+		head->file.in = ft_strdup(name);
+	else
 	{
-		path_bar = ft_strjoin(paths[j], "/");
-		complete_path = ft_strjoin(path_bar, command);
-		if (!access(complete_path, F_OK))
-			return (complete_path);
-		free(path_bar);
-		free(complete_path);
-		j++;
+		head->file.out = ft_strdup(name);
+		if (head->is_heredoc)
+			head->file.outflag = O_WRONLY | O_CREAT | O_APPEND;
+		else
+			head->file.outflag = O_WRONLY | O_CREAT;
 	}
-	return (NULL);
 }
 
-static char	**get_path(char *env[])
+static t_cmd	*get_and_handle_commands(t_head *head, int ac, char *av[], char **paths)
 {
-	const char	*prefix = "PATH=";
-	int		i;
+	const char	*here_doc = "here_doc";
+	t_cmd		*commands;
+	int			j;
+	int			i;
 
-	i = 0;
-	while (env[i])
-	{
-		if (!ft_strncmp(env[i], prefix, ft_strlen(prefix)))
-			return (ft_split(ft_strlen(prefix) + env[i], ':'));
-		i++;
-	}
-	return (NULL);
-}
-
-static int	get_amount(int ac, char *av[])
-{
-	int	amount;
-	int	i;
-
-	amount = 0;
-	i = 0;
-	while (i < ac)
-	{
-		if (access(av[i], F_OK))
-			amount += 1;
-		i++;
-	}
-	return (amount);
-}
-
-static t_cmd	get_cmd(char *command, char **paths)
-{
-	t_cmd	cmd;
-
-	cmd.args = ft_split(command, ' ');
-	cmd.path = get_valid_path(paths, cmd.args[0]);
-	return (cmd);
-}
-
-static t_cmd	*get_commands(t_head *head, int ac, char *av[], char **paths)
-{
-	t_cmd	*commands;
-	int		j;
-	int		i;
-
-	commands = ft_calloc(get_amount(ac, av) + 1, sizeof(t_cmd *));
+	head->cmds_size = get_cmds_amount(ac, av);
+	commands = ft_calloc(head->cmds_size + 1, sizeof(t_cmd));
 	if (!commands)
 		return (NULL);
 	j = 0;
 	i = 0;
 	while (i < ac)
 	{
-		if (!access(av[i], F_OK | R_OK | W_OK))
-		{
-			if (!head->file.in)
-				head->file.in = ft_strdup(av[i]);
-			else
-				head->file.out = ft_strdup(av[i]);
-		}
+		if (i == 0 && !ft_strncmp(av[i], here_doc, ft_strlen(here_doc)))
+			head->is_heredoc = TRUE;
+		else if (head->is_heredoc && i == 1)
+			head->limiter = av[i];
+		else if (!access(av[i], F_OK) || (access(av[i], F_OK) && i == ac - 1))
+			set_file(head, av[i], i);
 		else
-			commands[j++] = get_cmd(av[i], paths);
+			commands[j++] = get_cmd(head, av[i], paths);
 		i++;
 	}
-	commands[j].path = NULL;
 	return (commands);
 }
 
-void	parse(t_head *head, int ac, char *av[], char *ev[])
+void	parse(t_head **head, int ac, char *av[], char *ev[])
 {
-	head->cmds = get_commands(head, ac, av, get_path(ev));
+	char	**paths;
+	int		i;
+
+	if (ac < 2)
+		return ;
+	(*head) = ft_calloc(1, sizeof(t_head));
+	if (!(*head))
+		end(*head, 1, "head struct");
+	paths = get_paths(ev);
+	(*head)->cmds = get_and_handle_commands(*head, ac, av, paths);
+	i = 0;
+	while (paths[i] != NULL)
+		free(paths[i++]);
+	free(paths);
 }
