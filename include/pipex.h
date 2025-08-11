fdc5138a7d4f9cf35444c715caeb326d30ae36bf
denionline <danielximenes164@gmail.com>
2025-08-11 11:38:23 +0100

    refactor: Update struct definitions and function signatures in pipex.h

--- include/pipex.h
+++ include/pipex.h
@@ -6,7 +6,7 @@
 /*   By: dximenes <dximenes@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/31 15:36:38 by dximenes          #+#    #+#             */
-/*   Updated: 2025/08/06 16:53:23 by dximenes         ###   ########.fr       */
+/*   Updated: 2025/08/11 11:23:33 by dximenes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,28 +20,51 @@
 #include <sys/wait.h>
 #include <string.h>
 
+#define COMMANDS_LIMIT 1024
+#define OUT 1
+#define IN 0
+
+typedef struct s_fd
+{
+	int	in;
+	int	out;
+}	t_fd;
+
 typedef struct s_cmd
 {
 	char	**args;
 	char	*path;
 }	t_cmd;
 
-typedef struct s_path
+typedef struct s_file
 {
 	char	*in;
 	char	*out;
-}	t_path;
+	int		outflag;
+}	t_file;
 
 typedef struct s_head
 {
-	t_path	file;
+	t_file	file;
 	t_cmd	*cmds;
+	int		cmds_size;
+	int		is_heredoc;
+	char	*limiter;
 }	t_head;
 
 // parse.c
-void	parse(t_head *head, int ac, char *av[], char *ev[]);
+void	parse(t_head **head, int ac, char *av[], char *ev[]);
 
-// // commands.c
-// char	**get_commands(t_head *head, int ac, char *av[]);
+// commands.c
+void	commands(t_head *head, char *envp[]);
+
+// end.c
+void	end(t_head *head, int status, char *description);
+
+t_fd	get_fdfile(t_head *head);
+char	*get_valid_path(char **paths, char *command);
+char	**get_paths(char *env[]);
+int		get_cmds_amount(int ac, char *av[]);
+t_cmd	get_cmd(t_head *head, char *command, char **paths);
 
 #endif
