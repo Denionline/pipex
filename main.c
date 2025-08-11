da642935f36f437bc31715a17092eec0ce0b4b48
denionline <danielximenes164@gmail.com>
2025-08-11 11:37:48 +0100

    refactor: Simplify main function and update header inclusion

--- main.c
+++ main.c
@@ -6,36 +6,17 @@
 /*   By: dximenes <dximenes@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/31 15:36:25 by dximenes          #+#    #+#             */
-/*   Updated: 2025/08/08 11:55:28 by dximenes         ###   ########.fr       */
+/*   Updated: 2025/08/11 11:26:05 by dximenes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "include/pipex.h"
+#include "pipex.h"
 
-int	main(int argc, char *argv[], char *envp[])
+int main(int argc, char *argv[], char *envp[])
 {
-	t_head *head;
-	
-	if (argc < 2)
-		return (1);
-	head = ft_calloc(1, sizeof(t_head));
-	if (!head)
-		return (1);
-	parse(head, argc - 1, argv + 1, envp);
+	t_head	*head;
 
-	int	fd_infile = open(head->file.in, O_RDONLY);
-	int	fd_outfile = open(head->file.out, O_WRONLY | O_CREAT, 0777);
-	int	pipefd[2];
-
-	pipe(pipefd);
-	int	id = fork();
-
-	dup2(fd_outfile, 1);
-	if (id == 0)
-	{
-		dup2(fd_infile, 0);
-		execve(head->cmds[0].path, head->cmds[0].args, NULL);
-	}
+	parse(&head, argc - 1, argv + 1, envp);
+	commands(head, envp);
+	end(head, 0, "SUCESS");
 }
-
-// ./pipex infile "grep M" "wc -l" outfile
