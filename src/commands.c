0dfe6af24a2fd8be05a57025304e36d25657d8be
denionline <danielximenes164@gmail.com>
2025-08-15 16:17:01 +0100

    feat: Updating the fixes

--- src/commands.c
+++ src/commands.c
@@ -6,7 +6,7 @@
 /*   By: dximenes <dximenes@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/04 10:33:57 by dximenes          #+#    #+#             */
-/*   Updated: 2025/08/14 17:57:09 by dximenes         ###   ########.fr       */
+/*   Updated: 2025/08/15 10:39:27 by dximenes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ static int	exec_cmd(int in, int out, t_cmd command, char **envp)
 
 	if (pid == 0)
 	{
-		// ft_printf("Execucao do comando %s no STDIN=%d & STDOUT=%d\n", command.args[0], in, out);
+		// ft_printf("Cmd: %s & Path: %s | STDIN=%d & STDOUT=%d\n", command.args[0], command.path, in, out);
 		dup2(in, STDIN_FILENO);
 		dup2(out, STDOUT_FILENO);
 		close_both(in, out);
@@ -40,16 +40,19 @@ void	commands(t_head *head, char *envp[])
 	int			pipefd[2];
 	int			in_fd;
 	int			i;
+	int			is_sucess;
 
 	in_fd = get_and_handle_heredoc(head, filefd.in);
 	i = 0;
 	while (i < head->cmds_size - 1)
 	{
 		pipe(pipefd);
-		if (exec_cmd(in_fd, pipefd[OUT], head->cmds[i], envp))
+		is_sucess = exec_cmd(in_fd, pipefd[OUT], head->cmds[i], envp);
+		ft_printf("%d\n");
+		if (is_sucess)
 		{
 			ft_printf("Errado\n");
-			end(head, 2, head->cmds[i].path);
+			end(head, 2, head->cmds[i].args[0]);
 		}
 		close(pipefd[OUT]);
 		if (in_fd != filefd.in)
